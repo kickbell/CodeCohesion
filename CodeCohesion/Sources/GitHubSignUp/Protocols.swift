0233e77fa4c9fc1fc989b98d23267924dc67615d
kickbell <dev.kickbell@gmail.com>
2022-07-08 00:31:33 +0900

    Add Protocols

---
+++ CodeCohesion/Sources/GitHubSignUp/Protocols.swift
@@ -0,0 +1,42 @@
+//
+//  Protocols.swift
+//  CodeCohesion
+//
+//  Created by jc.kim on 7/7/22.
+//
+
+import RxSwift
+import RxCocoa
+
+enum ValidationResult {
+    case ok(message: String)
+    case empty
+    case validating
+    case failed(message: String)
+}
+
+enum SignupState {
+    case signedUp(signedUp: Bool)
+}
+
+protocol GitHubAPI {
+    func usernameAvailable(_ username: String) -> Observable<Bool>
+    func signup(_ username: String, password: String) -> Observable<Bool>
+}
+
+protocol GitHubValidationService {
+    func validateUsername(_ username: String) -> Observable<ValidationResult>
+    func validatePassword(_ password: String) -> ValidationResult
+    func validateRepeatedPassword(_ password: String, repeatedPassword: String) -> ValidationResult
+}
+
+extension ValidationResult {
+    var isValid: Bool {
+        switch self {
+        case .ok:
+            return true
+        default:
+            return false
+        }
+    }
+}
