64df30459ae24297778af717da7254b0438a38e9
devjck <jck0518@cashwalk.io>
2022-07-08 17:12:35 +0900

    Update CustomPickerViewController

--- CodeCohesion/Sources/UIPickerView/CustomPickerViewAdapterExampleViewController.swift
+++ CodeCohesion/Sources/UIPickerView/CustomPickerViewAdapterExampleViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class CustomPickerViewAdapterExampleViewController: UIViewController {
     
@@ -13,10 +16,19 @@ class CustomPickerViewAdapterExampleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bind()
     }
     
-
+    private func bind() {
+        Observable.just([[1, 2, 3],[5, 8, 13],[21, 34]])
+            .bind(to: pickerView.rx.items(adapter: PickerViewViewAdapter()))
+            .disposed(by: rx.disposeBag)
+        
+        pickerView.rx.modelSelected(Int.self)
+            .subscribe(onNext: { models in
+                print(models)
+            })
+            .disposed(by: rx.disposeBag)
+    }
 
 }
