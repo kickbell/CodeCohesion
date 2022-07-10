e811925bed74dc8ce253c7f9c762c85ab773586b
kickbell <dev.kickbell@gmail.com>
2022-07-11 06:48:54 +0900

    Add WikipediaImageCell

---
+++ CodeCohesion/Sources/WikipediaSearch /WikipediaImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  WikipediaImageCell.swift
+//  CodeCohesion
+//
+//  Created by jc.kim on 7/11/22.
+//
+
+import UIKit
+
+class WikipediaImageCell: UICollectionViewCell {
+    
+    @IBOutlet var imageOutlet: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
