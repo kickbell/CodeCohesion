6b5fbb875c26b33ca333d7fc03261dc749004599
kickbell <dev.kickbell@gmail.com>
2022-07-12 11:59:26 +0900

    Update WikipodiaImageCell

--- CodeCohesion/Sources/WikipediaSearch /Views/WikipediaImageCell.swift
+++ CodeCohesion/Sources/WikipediaSearch /Views/WikipediaImageCell.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class WikipediaImageCell: UICollectionViewCell {
     
     @IBOutlet var imageOutlet: UIImageView!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    var downloadableImage: Observable<DownloadableImage>? {
+        didSet {
+            self.downloadableImage?
+                .asDriver(onErrorJustReturn: DownloadableImage.offlinePlaceholder)
+                .drive(imageOutlet.rx.downloadableImageAnimated(CATransitionType.fade.rawValue))
+                .disposed(by: rx.disposeBag)
+        }
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadableImage = nil
+    }
+
+}
+
+enum DownloadableImage{
+    case content(image: UIImage)
+    case offlinePlaceholder
+}
+
+
+extension Reactive where Base: UIImageView {
+
+    var downloadableImage: Binder<DownloadableImage>{
+        downloadableImageAnimated(nil)
+    }
+
+    func downloadableImageAnimated(_ transitionType: String?) -> Binder<DownloadableImage> {
+        return Binder(base) { imageView, image in
+            for subview in imageView.subviews {
+                subview.removeFromSuperview()
+            }
+            switch image {
+            case .content(let image):
+                (imageView as UIImageView).rx.image.on(.next(image))
+            case .offlinePlaceholder:
+                let label = UILabel(frame: imageView.bounds)
+                label.textAlignment = .center
+                label.font = UIFont.systemFont(ofSize: 35)
+                label.text = "⚠️"
+                imageView.addSubview(label)
+            }
+        }
+    }
 }
