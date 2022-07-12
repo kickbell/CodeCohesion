3924349ab399c3daf03edb2465e3dead6cb6ed30
kickbell <dev.kickbell@gmail.com>
2022-07-13 01:13:04 +0900

    Add UINavigationController+Extensions

--- CodeCohesion/Sources/GitHubSearchRepositories/UINavigationController+Extensions.swift
+++ CodeCohesion/Sources/GitHubSearchRepositories/UINavigationController+Extensions.swift
@@ -5,4 +5,19 @@
 //  Created by jc.kim on 7/13/22.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct Colors {
+    static let offlineColor = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0)
+    static let onlineColor = nil as UIColor?
+}
+
+extension Reactive where Base: UINavigationController {
+    var isOffline: Binder<Bool> {
+        return Binder(base) { navigationController, isOffline in
+            navigationController.navigationBar.barTintColor = isOffline ? Colors.offlineColor : Colors.onlineColor
+        }
+    }
+}
