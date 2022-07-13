46344659ea5e43483c894b890548e9b38e1f57ed
kickbell <dev.kickbell@gmail.com>
2022-07-13 14:49:13 +0900

    Add Version

---
+++ CodeCohesion/Sources/GitHubSearchRepositories/Version.swift
@@ -0,0 +1,46 @@
+//
+//  Version.swift
+//  CodeCohesion
+//
+//  Created by ios on 2022/07/13.
+//
+
+
+import Foundation
+
+class Unique: NSObject {
+}
+
+struct Version<Value>: Hashable {
+
+    private let _unique: Unique
+    let value: Value
+
+    init(_ value: Value) {
+        self._unique = Unique()
+        self.value = value
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self._unique)
+    }
+
+    static func == (lhs: Version<Value>, rhs: Version<Value>) -> Bool {
+        lhs._unique === rhs._unique
+    }
+}
+
+extension Version {
+    func mutate(transform: (inout Value) -> Void) -> Version<Value> {
+        var newSelf = self.value
+        transform(&newSelf)
+        return Version(newSelf)
+    }
+
+    func mutate(transform: (inout Value) throws -> Void) rethrows -> Version<Value> {
+        var newSelf = self.value
+        try transform(&newSelf)
+        return Version(newSelf)
+    }
+}
+
