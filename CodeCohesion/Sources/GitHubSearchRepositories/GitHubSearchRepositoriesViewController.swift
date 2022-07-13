ef65a680a1d297c253f16a762b0ded5af3923b40
kickbell <dev.kickbell@gmail.com>
2022-07-13 16:41:54 +0900

    Update GitHubSearchRepositoriesViewController

--- CodeCohesion/Sources/GitHubSearchRepositories/GitHubSearchRepositoriesViewController.swift
+++ CodeCohesion/Sources/GitHubSearchRepositories/GitHubSearchRepositoriesViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 import RxDataSources
+import NSObject_Rx
 
 extension UIScrollView {
     func isNearBottomEdge(edgeOffset: CGFloat = 20.0) -> Bool {
@@ -33,17 +34,99 @@ class GitHubSearchRepositoriesViewController: UIViewController {
             return UITableViewCell()
         }, titleForHeaderInSection: { dataSource, sectionIndex in
             let section = dataSource[sectionIndex]
-            return section.items.count > 0 ? "Repositories (\(section.items.count)" : "No repositories found"
+            return section.items.count > 0 ? "Repositories (\(section.items.count))" : "No repositories found"
         }
     )
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        bind()
     }
     
+    private func bind() {
+        
+        let tableView: UITableView = self.tableView
+        let loadNextPageTrigger: (Driver<GitHubSearchRepositoriesState>) -> Signal<()> = { state in
+            tableView.rx.contentOffset.asDriver()
+                .withLatestFrom(state)
+                .flatMap { state in
+                    return tableView.isNearBottomEdge(edgeOffset: 20.0) && !state.shouldLoadNextPage ? Signal.just(()) : Signal.empty()
+                }
+        }
+        let activityIndicator = ActivityIndicator()
+        let searchBar: UISearchBar = self.searchBar
+        
+        let state = gitHubSearchRepositories(
+            searchText: searchBar.rx.text.orEmpty.changed.asSignal().throttle(.milliseconds(300)),
+            loadNextPageTrigger: loadNextPageTrigger,
+            performSearch: { URL in
+                //api
+                return Observable.just(SearchRepositoriesResponse.success((repositories: [
+                    Repository(name: "hh", url: URL),
+                    Repository(name: "hh22", url: URL),
+                    Repository(name: "hh33", url: URL),
+                    Repository(name: "hh55", url: URL),
+                ], nextURL: nil)))
+            }
+        )
+        
+        state
+            .map { $0.repositories }
+            .distinctUntilChanged()
+            .map { [SectionModel(model: "Repositories", items: $0.value)] }
+            .drive(tableView.rx.items(dataSource: dataSource)) //이게 왜 되나 했는데, .mutationOne 같은 쪽에 inout 처리가 되어있어서 그러지 싶다.
+            .disposed(by: rx.disposeBag)
+        
+        state
+            .map { $0.isOffline }
+            .drive(navigationController!.rx.isOffline)
+            .disposed(by: rx.disposeBag)
+        
+        state
+            .map { $0.isLimitExceeded }
+            .distinctUntilChanged()
+            .filter { $0 }
+            .drive(onNext: { [weak self] n in
+                guard let self = self else { return }
+                
+                let message = "Exceeded limit of 10 non authenticated requests per minute for GitHub API. Please wait a minute. :(\nhttps://developer.github.com/v3/#rate-limiting"
+                
+                self.present(UIAlertController(title: "RxExample", message: message, preferredStyle: .alert), animated: true)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        tableView.rx.modelSelected(Repository.self)
+            .subscribe(onNext: { repository in
+                UIApplication.shared.open(repository.url)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        //keyboard disappear in scrolling
+        tableView.rx.contentOffset
+            .subscribe { _ in
+                if searchBar.isFirstResponder {
+                    _ = searchBar.resignFirstResponder()
+                }
+            }
+            .disposed(by: rx.disposeBag)
+        
+        tableView.rx.setDelegate(self)
+            .disposed(by: rx.disposeBag)
+        
+        activityIndicator
+            .drive(UIApplication.shared.rx.isNetworkActivityIndicatorVisible)
+            .disposed(by: rx.disposeBag)
+    }
     
-    
+    deinit {
+        self.navigationController?.navigationBar.backgroundColor = nil
+    }
+}
+
+
+extension GitHubSearchRepositoriesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
 }
