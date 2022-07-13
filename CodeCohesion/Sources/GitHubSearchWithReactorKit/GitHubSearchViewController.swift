8deb967859754cdd9ff38ec4cc5c34043ce9f868
kickbell <dev.kickbell@gmail.com>
2022-07-13 17:14:12 +0900

    Add GitHubSearchViewReactor

---
+++ CodeCohesion/Sources/GitHubSearchWithReactorKit/GitHubSearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GitHubSearchViewController.swift
+//  CodeCohesion
+//
+//  Created by ios on 2022/07/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+
+class GitHubSearchViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ CodeCohesion/Sources/GitHubSearchWithReactorKit/GitHubSearchViewReactor.swift
@@ -0,0 +1,28 @@
+//
+//  GitHubSearchViewReactor.swift
+//  CodeCohesion
+//
+//  Created by ios on 2022/07/13.
+//
+
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+final class GitHubSearchViewReactor: Reactor {
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState: State = State()
+    
+}
