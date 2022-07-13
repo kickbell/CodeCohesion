481d09fb17ecfd1b731cd397a120620bc374638b
kickbell <dev.kickbell@gmail.com>
2022-07-14 01:37:38 +0900

    Update Reactor

--- CodeCohesion/Sources/GitHubSearchWithReactorKit/GitHubSearchViewReactor.swift
+++ CodeCohesion/Sources/GitHubSearchWithReactorKit/GitHubSearchViewReactor.swift
@@ -12,15 +12,22 @@ import RxCocoa
 final class GitHubSearchViewReactor: Reactor {
     
     enum Action {
-        
+        case updateQuery(String?)
+        case loadNextPage
     }
     
     enum Mutation {
-        
+        case setQuery(String?)
+        case setRepos([String], nextPage: Int?)
+        case appendRepos([String], nextPage: Int?)
+        case setLoadingNextPage(Bool)
     }
     
     struct State {
-        
+        var query: String?
+        var repos: [String] = []
+        var nextPage: Int?
+        var isLoadingNextPage: Bool = false
     }
     
     let initialState: State = State()
