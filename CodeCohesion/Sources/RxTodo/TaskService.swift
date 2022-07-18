5ef84cecd3746d8882cd575e2c3e9b8d98da0524
kickbell <dev.kickbell@gmail.com>
2022-07-18 17:13:37 +0900

    Add TaskService

---
+++ CodeCohesion/Sources/RxTodo/TaskService.swift
@@ -0,0 +1,64 @@
+//
+//  TaskService.swift
+//  CodeCohesion
+//
+//  Created by ios on 2022/07/18.
+//
+
+import RxSwift
+
+enum TaskEvent {
+    case delete(id: String)
+    case move(id: String, to: Int)
+}
+
+protocol TaskServiceType {
+    var event: PublishSubject<TaskEvent> { get }
+    func fetchTasks() -> Observable<[Task]>
+    
+    func delete(taskID: String) -> Observable<Task>
+    func move(taskId: String, to: Int) -> Observable<Task>
+}
+
+final class TaskService: BaseService, TaskServiceType {
+    let event = PublishSubject<TaskEvent>.init()
+
+    func fetchTasks() -> Observable<[Task]> {
+        let defaultTasks: [Task] = [
+          Task(title: "Go to https://github.com/devxoul"),
+          Task(title: "Star repositories I am intersted in"),
+          Task(title: "Make a pull request"),
+        ]
+//        let defaultTaskDictionaries = defaultTasks.map { $0.asDictionary() }
+        return .just(defaultTasks)
+    }
+    
+    func saveTasks(_ tasks: [Task]) -> Observable<Void> {
+//      let dicts = tasks.map { $0.asDictionary() }
+//      self.provider.userDefaultsService.set(value: dicts, forKey: .tasks)
+      return .just(Void())
+    }
+    
+    func delete(taskID: String) -> Observable<Task> {
+        return self.fetchTasks()
+            .flatMap { [weak self] tasks -> Observable<Task> in
+                guard let `self` = self else { return .empty() }
+//                guard let index = tasks.firstIndex(where: { $0.id == taskID }) else { return .empty() }
+                var tasks = tasks
+                let deleteTask = tasks.removeLast()
+                return self.saveTasks(tasks).map { deleteTask }
+            }
+            .do(onNext: { task in
+                self.event.onNext(.delete(id: task.id))
+            })
+    }
+    
+    func move(taskId: String, to: Int) -> Observable<Task> {
+        self.fetchTasks()
+            .flatMap { tasks -> Observable<Task> in
+                let task = tasks.first!
+                return self.saveTasks([]).map { task }
+            }
+    }
+
+}
